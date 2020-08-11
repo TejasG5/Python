0e2007524074bf99751579466f4898b86450b255
TejasG5 <64637256+TejasG5@users.noreply.github.com>
2020-08-11 11:26:30 +0530

    Add files via upload
    This is a Binod detector which detects "binod" in .txt files.

---
+++ BINOD Detector/binod_main.py
@@ -0,0 +1,27 @@
+import os
+
+def findBinod(filename):
+    """"Detects Binod Hidden in files"""
+    with open(filename, "r") as f:
+        fileContent = f.read()
+    # Finds binod by converting striong to lowercase
+    if "binod" in fileContent.lower():
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    # Listing folder content
+    contents = os.listdir()
+    nBinod=0
+    for item in contents:
+        if item.endswith('txt'):
+            print(f"Detecting Binod in {item}")
+            flag = findBinod(item)
+            if(flag):
+                print(f"!!!!!Binod found in {item}!!!!!")
+                nBinod += 1
+            else:
+                print(f"Binod not found in {item}")
+    print("******Binod Detector Summary******")
+    print(f"{nBinod} files found with Binod hidden")
